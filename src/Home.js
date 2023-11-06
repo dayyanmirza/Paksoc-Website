c8adc68dcfc66c02cbdd57abe2fc5de3c6d5adc5
dayyanmirza <dayyanmirza15@gmail.com>
2023-11-06 19:36:25 +0000

    useState hooks example

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
+
 const Home = () => {
+    
+    const [name, setName] = useState('mario'); // if name is just called without the use of setName then mario is returned
+    const [age, setAge] = useState(25);
 
     const handleClick = () => {
-        console.log('hi there');
+        setName('dayyan'); // this changes the name to luigi 
+        setAge(20)
     } 
 
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click Me</button>
         </div>
     );
